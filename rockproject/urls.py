423503fc81b9a34ef6077303fdb6c6d1e47822f2
Michelle Totherow <tym4me13@gmail.com>
2023-11-08 13:51:25 -0600

    create type_view, update urls.py and package

--- rockproject/urls.py
+++ rockproject/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from rockapi.views import register_user, login_user
+from rockapi.views import TypeView
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'types', TypeView, 'type')
 
 urlpatterns = [
     path('', include(router.urls)),
